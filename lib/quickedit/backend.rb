2e529f5d4c4fc3ed9e5c7ab3fb92c10d9c0b2323
Magnus Holm <judofyr@gmail.com>
2010-10-12 22:59:30 +0200

    Insecure, ugly and hacky implementation. Work in progress.

--- lib/quickedit/backend.rb
+++ lib/quickedit/backend.rb
@@ -1,4 +1,16 @@
 class Quickedit
   class Backend
+    def call(env)
+      request = Rack::Request.new(env)
+      params = request.params
+      case params['action']
+      when 'save'
+        adapter_klass, type, id, key = Marshal.load(URI.decode(params['instance']))
+        adapter = adapter_klass.find(type, id)
+        adapter.update(key, params['value'])
+      end
+      
+      [200, {'Content-Type' => 'text/html'}, []]
+    end
   end
 end
